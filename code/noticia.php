04e87cbcbc832aacf19b7536aaf9945bedda4c43
Jhadith Noboa <sotogalas@gmail.com>
2026-05-03 20:01:07 -0500

    noticias-agregado

---
+++ code/noticia.php
@@ -0,0 +1,31 @@
+
+<html lang="en">
+<head>
+    <title>Noticias</title>
+</head>
+<body>
+    
+    <div style="width: 100%; height:5% ;background-color: rgb(52, 61, 94); float: left;"></div>
+    <div style="width: 100%; height:auto ;background-color: rgb(231, 234, 235); float: left;">
+        <center style="color: rgb(52, 61, 94); font-size: 25px; padding-top: 10px;"><b>Noticias Destacadas</b></center>
+        <div class="tarjeta">
+            <h2 style="color: rgb(52, 61, 94);">AMD: Nueva serie Ryzen AI 400 y MI400</h2>
+            <p style="color: rgb(52, 61, 94);">AMD ha dado un golpe sobre la mesa en el CES 2026 y eventos posteriores con lanzamientos enfocados totalmente en IA local y centros de datos.</p>
+            <a href="https://www.pccomponentes.com/amd-ces-2026-ryzen-procesadores-ia">Ver mas</a>
+        </div>
+
+        <div class="tarjeta">
+            <h2 style="color: rgb(52, 61, 94);">NVIDIA: Preparando el salto a "Vera Rubin"</h2>
+            <p style="color: rgb(52, 61, 94);">Mientras las GPU Blackwell Ultra (B300) dominan el mercado actual, NVIDIA ya está moviendo piezas para su próxima arquitectura.</p>
+            <a href="https://www.profesionalreview.com/2025/02/28/nvidia-blackwell-ultra-2025-vera-rubin-2026/">Ver mas</a>
+        </div>
+
+        <div class="tarjeta">
+            <h2 style="color: rgb(52, 61, 94);">Intel: Recuperación gracias a la IA y Foundry</h2>
+            <p style="color: rgb(52, 61, 94);">Intel ha reportado un crecimiento sólido en el primer trimestre de 2026 impulsado por su división de centros de datos.</p>          
+           <a href="https://www.tweaktown.com/news/111309/intel-is-now-trying-to-keep-up-with-ai-demand-by-using-scrap-dies/index.html">Ver mas</a>
+        </div>
+
+    </div>
+</body>
+</html>
